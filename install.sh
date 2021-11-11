1551112b44a74e8f73c6ed020651c2b58e7c4965
Max-42 <max@oppermann.fun>
2021-11-11 16:27:42 +0100

    get build args from .env

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo"You may not execute this, but u can copy paste <3 thx ;)"
 echo"docker volume create --name=tobi_server_data"
-echo"docker-compose --env-file .env up --build-arg sshrootpassword=root:y0urR34lYSecuReP4SsWoRD"
+echo"docker-compose --env-file .env up"
 exit #prevents execution
 
 docker volume create --name=tobi_server_data
-docker-compose --env-file .env up --build-arg sshrootpassword=root:y0urR34lYSecuReP4SsWoRD
+docker-compose --env-file .env up
